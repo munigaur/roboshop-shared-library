405ade5d5c4380b7f5c372bf63630c81e4ebb2c7
munigaur <munindragaur@gmail.com>
2023-05-03 17:32:38 +0530

    muni1

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -8,12 +8,16 @@ def lintChecks(COMPONENT) {
 
 def sonarChecks(COMPONENT) {
        sh "echo starting Code Quality Analysis"
-       sh "sonar-scanner -Dsonar.projectKey=catalogue -Dsonar.sources=. -Dsonar.host.url=http://3.235.100.37:9000 -Dsonar.login=admin -Dsonar.password=password"
+       sh "sonar-scanner -Dsonar.projectKey=catalogue -Dsonar.sources=. -Dsonar.host.url=http://${SONAR_URL}:9000 -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}"
      }
 
 def call(COMPONENT){
 pipeline {
     agent any
+    environment {
+      SONAR=credentials('SONAR')
+      SONAR_URL="3.235.100.37"
+    }
     stages {
         stage('Lint Checks') {
           steps {

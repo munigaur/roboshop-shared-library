d79af0fecf89af7a640d8e5a4c3a40e0e55cf4c6
munigaur <munindragaur@gmail.com>
2023-05-04 13:33:16 +0530

    muni1

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -6,12 +6,6 @@ def lintChecks(COMPONENT) {
        sh "echo LINT CHECKS COMPLETED !!!"
      }
 
-def sonarChecks(COMPONENT) {
-       sh "echo starting Code Quality Analysis"
-       sh "sonar-scanner -Dsonar.projectKey=catalogue -Dsonar.sources=. -Dsonar.host.url=http://${SONAR_URL}:9000 -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}"
-       sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > sonar-quality-gata.sh"
-       sh "bash -x sonar-quality-gata.sh  ${SONAR_USR} ${SONAR_PSW} ${SONAR_URL} ${COMPONENT}"     
-     }
 
 def call(COMPONENT){
 pipeline {

8448bf48cdcafdf664cb3d13b6440fb6b575877c
munigaur <munindragaur@gmail.com>
2023-05-04 11:11:34 +0530

    muni1

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -6,7 +6,7 @@ def lintChecks(COMPONENT) {
 def sonarChecks(COMPONENT) {
        sh "echo starting Code Quality Analysis"
        sh "mvn clean compile"
-       sh "sonar-scanner -Dsonar.projectKey=catalogue -Dsonar.sources=. -Dsonar.host.url=http://${SONAR_URL}:9000 -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}"
+       sh "sonar-scanner -Dsonar.projectKey=catalogue -Dsonar.java.binaries=target/ -Dsonar.host.url=http://${SONAR_URL}:9000 -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}"
        //sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > sonar-quality-gata.sh"
        //sh "bash -x sonar-quality-gata.sh  ${SONAR_USR} ${SONAR_PSW} ${SONAR_URL} ${COMPONENT}"     
      }

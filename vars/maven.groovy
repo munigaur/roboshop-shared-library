5267919433e73e142f030b70954f043cbbc92ac1
munigaur <munindragaur@gmail.com>
2023-05-04 10:15:18 +0530

    muni1

---
+++ vars/maven.groovy
@@ -0,0 +1,42 @@
+def lintChecks(COMPONENT) {
+       sh "echo installing mvn"
+       sh "echo LINT CHECKS COMPLETED !!!"
+     }
+
+def sonarChecks(COMPONENT) {
+       sh "echo starting Code Quality Analysis"
+       sh "sonar-scanner -Dsonar.projectKey=catalogue -Dsonar.sources=. -Dsonar.host.url=http://${SONAR_URL}:9000 -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}"
+       sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > sonar-quality-gata.sh"
+       sh "bash -x sonar-quality-gata.sh  ${SONAR_USR} ${SONAR_PSW} ${SONAR_URL} ${COMPONENT}"     
+     }
+
+def call(COMPONENT){
+pipeline {
+    agent any
+    environment {
+      SONAR=credentials('SONAR')
+      SONAR_URL="34.200.223.159"
+    }
+    stages {
+        stage('Lint Checks') {
+          steps {
+             script {
+                 lintChecks(COMPONENT)
+                 }
+             }
+          }
+        stage('sonar Checks') {
+          steps {
+             script {
+                 sonarChecks(COMPONENT)
+                 }
+             }
+          }
+         stage ('downloading the dependencies') {
+            steps {
+        	   sh "mvn clean package"
+      		}
+   		 }
+ 	}
+}
+}
